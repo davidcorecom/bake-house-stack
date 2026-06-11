83ae87795ecb59bbb6c2c55edfd9b336a75f2c5c
David Carr <d.carr@corecomtechacademy.co.uk>
2026-06-11 15:17:02 +0100

    deploy

--- aws-iac-07-dynamoDB/exercise/bakehouse-app/functions/health-check.js
+++
@@ -1,6 +0,0 @@
-export const healthcheckHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ status: 'ok' })
-  }
-}
--- aws-iac-07-dynamoDB/exercise/bakehouse-app/functions/redirects.js
+++
@@ -1,9 +0,0 @@
-// functions/redirects.js
-// Minimal CloudFront Function: just passes the request through as it is
-
-function handler(event) {
-    var request = event.request
-  
-    // Later we can add nice redirect logic here (eg for React routes)
-    return request
-  }
